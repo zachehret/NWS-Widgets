5e53cc5a1aaf84a4585d3a505a7540fe988f890f
Zach Ehret <zachehret@outlook.com>
2021-11-15 02:56:19 +0000

    Added the cache constant to file paths

--- nwswidgets/utilities/CacheManager.php
+++ nwswidgets/utilities/CacheManager.php
@@ -4,16 +4,17 @@ namespace nwswidgets\data\utilities;
 
 class CacheManager
 {
+    public const CACHE = __DIR__ . "/../_cache/";
     /**
      * Determines if the file is due to be updated aka the cached version is expired.
      *
-     * @param string $file - The path to the file
+     * @param string $file - The path to the file relative to CacheManager::CACHE
      * @param int $maxAge - The max age of the file in seconds
      * @return bool - Returns true if the file is expired, does not exist, or is not accessible; Returns false otherwise.
      */
     public static function needsUpdated(string $file, int $maxAge) : bool {
-        if(file_exists($file)) {
-            $lastUpdatedTime = filemtime($file);
+        if(file_exists(CacheManager::CACHE . $file)) {
+            $lastUpdatedTime = filemtime(CacheManager::CACHE . $file);
             if($lastUpdatedTime === false) {
                 // Failure to read
                 return true;
